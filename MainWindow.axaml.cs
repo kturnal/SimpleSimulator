50ff8266533a3e98e30c9751f47b86bfc5076353
kturnal <batuker.kagan@windowslive.com>
2025-02-23 20:14:51 +0300

    added simulator message in real time

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -3,6 +3,8 @@ using Avalonia.Controls;
 using Avalonia.Controls.Shapes;
 using Avalonia.Markup.Xaml;
 using Avalonia.Media;
+using Avalonia.Threading;
+//using UIThread = Avalonia.Threading.Dispatcher.UIThread;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -12,11 +14,12 @@ namespace SimpleSimulator
     public partial class MainWindow : Window
     {
 
-        private TextBox speedInput;
-        private TextBox angleInput;
-        private TextBox heightInput;
-        private Button simulateButton;
-        private Canvas canvas;
+        private TextBox? speedInput;
+        private TextBox? angleInput;
+        private TextBox? heightInput;
+        private Button? simulateButton;
+        private Canvas? canvas;
+        private TextBlock? simulationDataLabel;
 
 
         public MainWindow()
@@ -27,17 +30,23 @@ namespace SimpleSimulator
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
-            speedInput = this.FindControl<TextBox>("SpeedInput");
-            angleInput = this.FindControl<TextBox>("AngleInput");
-            heightInput = this.FindControl<TextBox>("HeightInput");
-            simulateButton = this.FindControl<Button>("SimulateButton");
-            canvas = this.FindControl<Canvas>("SimulationCanvas");
+            speedInput = this.FindControl<TextBox>("SpeedInput")!;
+            angleInput = this.FindControl<TextBox>("AngleInput")!;
+            heightInput = this.FindControl<TextBox>("HeightInput")!;
+            simulateButton = this.FindControl<Button>("SimulateButton")!;
+            canvas = this.FindControl<Canvas>("SimulationCanvas")!;
+            simulationDataLabel = this.FindControl<TextBlock>("SimulationDataLabel")!;
+
+            //fill with initial values for fast testing
+            speedInput.Text = "20";
+            angleInput.Text = "60";
+            heightInput.Text = "10";
             
             simulateButton.Click += OnSimulateClicked;
         }
 
         /// <summary>
-        /// 
+        /// Event that triggers when the simulate button is clicked.
         /// </summary>
         /// <param name="sender">Sender object that subscribes to the event.</param>
         /// <param name="e">Event arguments.</param>
@@ -72,7 +81,7 @@ namespace SimpleSimulator
 
             List<Ellipse> projectiles = new List<Ellipse>();
 
-            canvas.Children.Clear();
+            canvas.Children.Clear(); // NRE possibility? 
 
             while (true)
             {
@@ -93,10 +102,20 @@ namespace SimpleSimulator
                 Canvas.SetTop(projectile, 300 - (y * 5)); // Invert Y-axis for correct display
                 canvas.Children.Add(projectile);
                 projectiles.Add(projectile);
+
+                Avalonia.Threading.Dispatcher.UIThread.Post(() =>
+                {
+                    simulationDataLabel!.Text = $"Time: {time:F2}s | X: {x:F2}m | Y: {y:F2}m";
+                });
                 
                 await Task.Delay(50);
                 time += timeStep;
             }
+
+            Avalonia.Threading.Dispatcher.UIThread.Post(() =>
+            {
+                simulationDataLabel!.Text = "Simulation Complete!";
+            });             
         }
     }
 }
